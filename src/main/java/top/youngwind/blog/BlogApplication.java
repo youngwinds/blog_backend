f21be831d6c4994b0999eca9da934dad43d1fc13
yangjiangtao <yangjiangtao@bytedance.com>
2021-10-13 14:36:42 +0800

    feat: add swagger3

--- src/main/java/top/youngwind/blog/BlogApplication.java
+++ src/main/java/top/youngwind/blog/BlogApplication.java
@@ -3,8 +3,10 @@ package top.youngwind.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @EnableJpaAuditing
+@EnableOpenApi
 @SpringBootApplication
 public class BlogApplication {
 
